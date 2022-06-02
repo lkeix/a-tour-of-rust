0165c443007464b755242ee356728e931ba99aa6
lkeix <ai.kanaria.ai@gmail.com>
2022-06-02 21:11:29 +0900

    add: enum

---
+++ section3/enum/main.rs
@@ -0,0 +1,33 @@
+#![allow(dead_code)]
+
+enum Species {
+  Crab,
+  Octopus,
+  Fish,
+  Clam
+}
+
+struct SeaCreature {
+  species: Species,
+  name: String,
+  arms: i32,
+  legs: i32,
+  weapon: String,
+}
+
+fn main() {
+  let ferris = SeaCreature{
+    species: Species::Crab,
+    name: String::from("Ferris"),
+    arms: 2,
+    legs: 4,
+    weapon: String::from("claws"),
+  };
+
+  match ferris.species {
+    Species::Crab => println!("{} is a crab", ferris.name),
+    Species::Octopus => println!("{} is an octopus", ferris.name),
+    Species::Fish => println!("{} is a fish", ferris.name),
+    Species::Clam => println!("{} is a Calm", ferris.name)
+  }
+}
