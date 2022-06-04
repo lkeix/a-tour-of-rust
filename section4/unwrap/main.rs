8efc0f404cfd1dc800056fe89dd024763ee0eee3
lkeix <ai.kanaria.ai@gmail.com>
2022-06-04 11:46:01 +0900

    fix: unwrap

--- section4/unwrap/main.rs
+++ section4/unwrap/main.rs
@@ -8,6 +8,13 @@ fn do_something_that_might_fail(i: i64) -> Result<f32, String> {
 
 fn main() -> Result<(), String> {
   let v = do_something_that_might_fail(42).unwrap();
+  /*
+  do_something_that_might_fail(42).unwrap(); same
+  match do_something_that_might_fail(42) {
+    Ok(v) => v,
+    Err(e) => panic!(e)
+  }
+  */
 
   println!("{}", v);
 
